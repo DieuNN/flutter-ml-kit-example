37981568f266279433ca8b6615b05c1d9dcad73a
dieunn <nongngocdieu20122002@gmail.com>
2023-06-26 13:39:13 +0700

    Remove unsupported platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,15 @@
-import 'dart:developer';
-import 'dart:io';
-
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ml_kit/page/barcode_scanner/barcode_view.dart';
 import 'package:ml_kit/page/face_detector/face_detector_view.dart';
 import 'package:ml_kit/page/object_detector/object_detector_view.dart';
 import 'package:ml_kit/page/text_recognizer/text_recognizer_view.dart';
-import 'package:ml_kit/main.dart';
 import 'package:ml_kit/page/text_translation/text_translation_view.dart';
-import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
 
 List<CameraDescription> cameras = [];
 
-/// Physical devices only
-/// Everything copied from https://github.com/flutter-ml/google_ml_kit_flutter/tree/master/packages/google_ml_kit/example
-
-Future<List<dynamic>> encode(String input) async {
-  const String regex = "r'[^p{L}p{N}]'";
-  final newString = input.replaceAll(RegExp(regex), '');
-  log("$newString");
-  List<dynamic> result = List.filled(1 * 64, 0);
-  result = newString.characters.map((e) => e.codeUnitAt(0)).toList();
-  List<int> wrapper = List.filled(1 * 64, 0).flatten();
-
-  for (var i = 0; i < result.length; i++) {
-    wrapper[i] = result[i];
-  }
-
-  return wrapper.reshape([1, 64]).toList();
-}
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
 
   cameras = await availableCameras();
@@ -47,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: kIsWeb ? const Text("Web is not supported") : const HomePage(),
       theme: ThemeData(useMaterial3: true),
     );
   }
